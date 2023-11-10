760e041a6065422f8adf2a597d9a28d4317f63a1
Genesis Chrome <49604988+ChromeGenesis@users.noreply.github.com>
2023-11-10 06:22:09 +0100

    Add files via upload

---
+++ menu_example.py
@@ -0,0 +1,18 @@
+import customtkinter as ctk
+import customMenu
+
+root = ctk.CTk()
+root.title("Custom Menu")
+root.geometry("300x200")
+
+# Create a menu bar
+menu = customMenu.Menu(root, _relheight=0.1)
+
+# Create a file menu
+file_menu = menu.menu_bar(text="File", tearoff=0)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+root.mainloop()
